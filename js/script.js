45da5046a02c93eea060a691988da94c23dcf83c
Gabriel Cavalcanti Martins <treedrewlife@gmail.com>
2026-05-16 15:13:32 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', () => {
        
         document.body.style.opacity = '0';
         setTimeout(() => {
-            window.location.href = './Pages/camera1.html'; 
+            window.location.href = './pages/camera1.html'; 
         }, 500);
     });
 
@@ -47,7 +47,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     btnCreditos.addEventListener('click', () => {
-        window.location.href = './Pages/creditos.html';
+        window.location.href = './pages/creditos.html';
     });
 
     
